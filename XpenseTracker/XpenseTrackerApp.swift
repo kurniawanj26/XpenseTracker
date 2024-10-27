2bf3aa6ce5842bb1ccfc87e522b697e81a17c42f
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2024-10-27 17:11:48 +0700

    Initial Commit

---
+++ XpenseTracker/XpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpenseTrackerApp.swift
+//  XpenseTracker
+//
+//  Created by Jayadi Kurniawan on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct XpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
